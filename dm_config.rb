e1dfa8a9f805bb3c82059cb93c51a3ebda558e66
Kshitij Karthick <kshitij.karthick@gmail.com>
2014-09-29 22:49:49 +0530

    Models Creation using Data Mapper and Storage in a Simple SQLite Database

---
+++ dm_config.rb
@@ -0,0 +1,25 @@
+require 'dm-constraints'
+require_relative './models/course'
+require_relative './models/department'
+require_relative './models/lecture_series'
+require_relative './models/enrollment'
+require_relative './models/exam'
+require_relative './models/faculty'
+require_relative './models/score'
+require_relative './models/student'
+require_relative './models/subject'
+require_relative './models/weekly_attendance'
+
+
+
+
+
+configure :development do
+	DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{File.dirname(File.expand_path(__FILE__))}/development.sqlite")
+ end
+ configure :production do
+ 	DataMapper.setup(:default, ENV['DATABASE_URL'])
+ end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
