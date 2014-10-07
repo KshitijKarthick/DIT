d676c127e9a7a70cf3e9ea2209b4f40ed84188f6
Kshitij Karthick <kshitij.karthick@gmail.com>
2014-10-07 20:57:04 +0530

    Data Retreival and Cleaning of Data based on the type

--- scripts/import.rb
+++ scripts/import.rb
@@ -13,11 +13,30 @@ class DDImporter
     @file.default_sheet = sheet_name
     @data_collection=Array.new
     header = file.row 1
-    header.map! {|header_name| header_name.chomp.downcase.strip}
+    header.map! {|header_name|clean header_name}
     2.upto(file.last_row) do |i|
       row = Hash[[header, file.row(i)].transpose]
       # row.delete "id"
+      row.keys.each do |key|
+        row[key]=clean row[key]
+      end
       @data_collection.push(row)
     end
+  end
+end
+def clean(data)
+  if data.class == String
+    return data.chomp.downcase.strip
+  elsif data.class == Fixnum
+    return data.to_s.chomp.downcase.strip
+  elsif data.class == Float
+    if data.round == data
+      return data.round
+    else
+      return data
+    end
+    #elsif data.class == NilClass
+    #  return ''
+    #
   end
 end
